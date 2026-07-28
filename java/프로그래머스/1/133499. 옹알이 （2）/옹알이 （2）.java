af8df8bd695d0cb326817cbf9bdb4d5fe14133a7
jjangjun0 <junyoung5915@gmail.com>
2026-07-29 04:15:26 +0900

    옹알이 - startsWith(string, start_idx) 함수 이용

--- java/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ java/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -9,22 +9,24 @@ class Solution {
             if (is_possible(babbling[i]))
                 count++;
         }
-        
+
         return count;
     }
-    
+
     boolean is_possible(String S) {
         boolean result = true;
-        
+
         int idx = 0;
         int last_say = -1;
         int L = S.length();
         boolean is_exist;
-        
+
         while (true) {
             is_exist = false;
             for (int i = 0; i < 4; i++) {
                 if (i != last_say && // 중복 X
+                        // idx + joka[i].length() <= L && // substring 가능
+                        // S.substring(idx, idx + joka[i].length()).equals(joka[i])) {
                         S.startsWith(joka[i], idx)) {  // joka로 시작
                     is_exist = true;
                     idx += joka[i].length();
@@ -32,16 +34,16 @@ class Solution {
                     break;
                 }
             }
-            
+
             if (idx == L)
                 break; // 딱 떨어지는 경우
-            
+
             if (!is_exist) {
                 result = false;
                 break;
             }
         }
-        
+
         return result;
     }
 }
